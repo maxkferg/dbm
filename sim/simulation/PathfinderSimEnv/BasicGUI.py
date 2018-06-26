7e602b685802be0bb56b980856f030406d16c16a
otgaard <otgaardm@gmail.com>
2018-06-26 17:05:08 +0200

    - Sized and displaying

--- sim/simulation/PathfinderSimEnv/BasicGUI.py
+++ sim/simulation/PathfinderSimEnv/BasicGUI.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Label
+import tkinter as tk
+from tkinter import Tk, Label, Canvas
 from PIL import ImageTk, Image
 
 
@@ -6,11 +7,13 @@ class DisplayWindow:
     def __init__(self, master):
         self.master = master
         master.title("PathfinderSim Display Window")
-        master.geometry("300x300")
+        master.geometry("256x256")
 
-        img = ImageTk.PhotoImage(Image.open('/Users/otgaard/Development/dbm/sim/assets/test2.png'))
-        panel = Label(master, image=img)
-        panel.pack(side="bottom", fill="both", expand="yes")
+        self.canvas = Canvas(master, width=256, height=256)
+        self.canvas.pack()
+
+        self.img = ImageTk.PhotoImage(image=Image.open('/Users/otgaard/Development/dbm/sim/assets/test2.png'))
+        self.canvas.create_image(0, 0, image=self.img, anchor=tk.NW)
 
 
 root = Tk()
