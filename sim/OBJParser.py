05e2eb78791358a6fa01d5def040da4e804ec929
otgaard <otgaardm@gmail.com>
2018-06-28 11:07:33 +0200

    - Added parser for OBJ

--- sim/OBJParser.py
+++ sim/OBJParser.py
@@ -4,11 +4,55 @@ import os
 
 class OBJParser:
     def __init__(self, filename):
-
-        pass
+        self.filename = filename
+        self.vertices = None
+        self.normals = None
+        self.texcoords = None
+        self.indices = None
 
     def parse(self):
-        pass
+        file = open(self.filename)
+
+        self.vertices = []
+        self.normals = []
+        self.texcoords = []
+        self.indices = []
+
+        for line in file:
+            if line[0:2] == 'v ':
+                els = line.split(' ')
+                self.vertices.append([
+                    float(els[1]),
+                    float(els[2]),
+                    float(els[3])
+                ])
+            elif line[0:3] == 'vn ':
+                els = line.split(' ')
+                self.normals.append(
+                    [float(els[1]),
+                     float(els[2]),
+                     float(els[3])
+                     ])
+            elif line[0:3] == 'vt ':
+                els = line.split(' ')
+                if len(els) == 3:
+                    self.texcoords.append([
+                        float(els[1]),
+                        float(els[2])
+                    ])
+                elif len(els) == 4:
+                    self.texcoords.append([
+                        float(els[1]),
+                        float(els[2]),
+                        float(els[3])
+                    ])
+            elif line[0:2] == 'f ':
+                els = line.split(' ')
+                self.indices.append([
+                    int(els[1].split('/')[0]) - 1,
+                    int(els[2].split('/')[0]) - 1,
+                    int(els[3].split('/')[0]) - 1
+                ])
 
     def get_prim_count(self):
         pass
