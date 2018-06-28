b1032f1bce34d950ca9608598212fe94b0d8e651
otgaard <otgaardm@gmail.com>
2018-06-28 09:49:04 +0200

    - Added resizing

--- sim/simulation/PathfinderSimEnv/BasicGUI.py
+++ sim/simulation/PathfinderSimEnv/BasicGUI.py
@@ -11,10 +11,15 @@ class DisplayWindow:
         master.geometry("512x512")
 
         self.canvas = Canvas(master, width=512, height=512)
-        self.canvas.pack()
+        self.canvas.pack(fill="both", expand=True)
 
-        self.img = ImageTk.PhotoImage(image=Image.open('/Users/otgaard/Development/dbm/sim/assets/test2.png'))
-        self.canvas.create_image(0, 0, image=self.img, anchor=tk.NW)
+        # Resize to fit the window
+        self.original = Image.open('/Users/otgaard/Development/dbm/sim/assets/test2.png')
+        self.image = self.original.resize((512, 512))
+        self.img = ImageTk.PhotoImage(image=self.image)
+
+        self.bk = self.canvas.create_image(256, 256, image=self.img, anchor=tk.CENTER)
+        self.canvas.bind('<Configure>', self.on_resize)
 
         self.ball_pos = [[randint(0,5), randint(0,5)], [randint(0,5), randint(0,5)]]
         self.balls = [
@@ -22,18 +27,25 @@ class DisplayWindow:
             self.canvas.create_oval(250, 250, 270, 270, fill="blue")
         ]
 
-    def update_display(self):
+    def on_update(self):
         for i in range(len(self.balls)):
             delta_x = randint(-1, 1)
             delta_y = randint(-1, 1)
             self.canvas.move(self.balls[i], delta_x, delta_y)
 
-        self.canvas.after(50, self.update_display)
+        self.canvas.after(50, self.on_update)
+
+    def on_resize(self, event):
+        print("Resize:", event.width, event.height)
+        self.image = self.original.resize((event.width, event.height))
+        self.img = ImageTk.PhotoImage(self.image)
+
+        self.bk = self.canvas.create_image(event.width/2, event.height/2, image=self.img, anchor=tk.CENTER)
 
 
 root = Tk()
 my_gui = DisplayWindow(root)
 
-my_gui.update_display()
+my_gui.on_update()
 
 root.mainloop()
