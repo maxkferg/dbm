ba8e5edef789c12ecc6f119ca15951d488634859
otgaard <otgaardm@gmail.com>
2018-08-10 09:43:19 +0200

    - Single-threaded tkinter working

--- sim/tools/PathfinderWindow.py
+++ sim/tools/PathfinderWindow.py
@@ -2,7 +2,7 @@ from tkinter import Tk, Canvas
 from random import randint, random
 import math
 import sys, os
-from multiprocessing import Process
+import multiprocessing as mp
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from tools.OBJModel import OBJModel
@@ -21,37 +21,79 @@ class PathfinderWindow:
     """This class displays a Tkinter window running in a separate process.  The Tkinter window contains a 2D top-down
     view of the model including the car, it's position, orientation, and visited grid blocks.
 
-    Note: The grid will automatically be the smallest unit size possible.  Thus, when analysing the input model, the
+    Note: The grid will automatically be the smallest unit size (GCD).  Thus, when analysing the input model, the
     2D grid will be configured to be made up of blocks measuring the smallest unit as the width of the narrowest floor
     polygon by the height of the narrowest tile.  Thus, if we have two rects of 2 x 10, 4 x 2, the grid size will be set
     to 2 x 2 to ensure the entire floor can be covered with a uniform tile size.
+
+    The GCD is computed in each axis to be the dimensions of the grid size.
     """
     def __init__(self, send_q, resp_q, floors, walls, is_test=True):
         """Initialise the PathfinderWindow with the floors and walls, both being paths because the OBJ files are
         loaded into a separate process in a separate python interpreter to avoid mixing two different GUI event loops.
         Set is_test equal to True when wishing to control the car with the keyboard."""
+        # Tkinter resources
         self.master = Tk()
         self.master.title("Grid Display")
         self.master.geometry("512x600")
+        self.is_test = is_test      # Allow single-threaded operation using keyboard
+        self.canvas = None
+        self.width = None           # Screen space width (i.e. canvas, not whole window)
+        self.height = None          # SS height
 
+        # Multiprocessing resources
         self.send_q = send_q        # The queue in which commands arrive for processing
         self.resp_q = resp_q        # The queue used to send responses to queries
+        self.shutdown_flag = False  # Used in mp mode to shutdown the system
 
+        # Model Resources
         self.wall_model = None      # The loaded OBJ file mdoel for the walls
         self.wall_bound = None      # The wall AABB world bound
-
         self.tile_grid = None       # TileGrid instance
 
-        if not self.build_tiles(floors):
+        self.car_pos = [0, 0]       # The position of the car in screen coordinates
+        self.car_orn = 0            # The orientation of the car in screen space
+        self.car_rays = 12          # Number of rays to simulate
+        self.ray_dtheta = 2.*math.pi/self.car_rays
+
+        if not self.setup_window():
+            print("Failed to setup Tkinter Display")
+            return
+
+        if not self.setup(floors, walls):
+            print("Failed to initialise Display Model")
             return
 
         self.on_update()
+        self.master.mainloop()
+
+    def setup_window(self):
+        self.master.bind('<Destroy>', self.on_destroy)
+        if self.is_test:
+            self.master.bind('<Left>', lambda x: self.cmd_turn_car(self.car_orn - .1))
+            self.master.bind('<Right>', lambda x: self.cmd_turn_car(self.car_orn + .1))
+            self.master.bind('<Up>', lambda x: self.cmd_move_car(m2d.add(self.car_pos, m2d.mul(m2d.make_polar(self.car_orn), 5))))
+            self.master.bind('<Down>', lambda x: self.cmd_move_car(m2d.add(self.car_pos, m2d.mul(m2d.make_polar(self.car_orn), -5))))
+
+        self.canvas = Canvas(self.master, width=512, height=512)
+        self.canvas.pack(fill="both", expand=True)
+        self.canvas.bind('<Configure>', self.on_resize)
+        self.width = self.canvas.winfo_width()
+        self.height = self.canvas.winfo_height()
+        return True
+
+    def setup(self, floor_file, wall_file):
+        if not self.build_tiles(floor_file):
+            return False
+
+        return True
 
     def build_tiles(self, floor_file):
         self.tile_grid = TileGrid(floor_file)
         if not self.tile_grid.is_valid():
             print("Failed to intialize TileGrid, aborting")
             return False
+
         return True
 
     def on_update(self):
@@ -83,21 +125,6 @@ class PathfinderWindow:
         """Draws the car each frame by updating the scene graph created by build_car."""
         pass
 
-    def cmd_move_car(self, target):
-        """Issue an async move command to the car.  Updates the current position of the car model in world space."""
-        pass
-
-    def cmd_turn_car(self, orn):
-        """Issue an async rotation command to the car.  Updates the current orientation of the car model."""
-        pass
-
-    def cmd_read_var(self, variable="car_pos"):
-        """Return the value of a variable asynchronously.
-        variable can be "car_pos" - the position of the car.
-                        "car_orn" = the orientation
-                        "screen"  = the dimensions of the Tkinter display."""
-        pass
-
     def has_response(self):
         """Returns whether or not there is a response for a read_var command in the response queue."""
         pass
@@ -110,8 +137,19 @@ class PathfinderWindow:
         """Shutdown the Tkinter application and resources."""
         pass
 
-    def on_destroy(self):
+    def on_destroy(self, arg):
         """Event triggered by closing window, etc. used to kill the thread and clean up the Tkinter resources."""
         pass
 
+    def cmd_move_car(self, target_vec):
+        self.send_q.put(["move", target_vec])
 
+    def cmd_turn_car(self, target_orn):
+        self.send_q.put(["turn", target_orn])
+
+    def cmd_read_var(self, variable="car_pos"): # car_pos, car_orn, or screen (dims)
+        self.send_q.put(["read", variable])
+
+
+if __name__ == '__main__':
+    pfw = PathfinderWindow(mp.Queue(), mp.Queue(), '../output/test2_floors.obj', '../output/test2_walls.obj', True)
--- sim/tools/TileGrid.py
+++ sim/tools/TileGrid.py
@@ -1,9 +1,11 @@
 import os
 import sys
+import functools
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from tools.OBJModel import OBJModel
 import tools.Math2D as m2d
+from math import gcd
 
 
 PA = 0
@@ -21,12 +23,13 @@ def compute_centre(AABB):
 
 # The TileGrid is an abstraction of the floor of the building so we can model it as a gridded surface that contains a
 # binary field indicating whether or not the tile has been "seen" by the robot (i.e. that the tile has intersected a
-# portion of the LIDAR rays triangles
+# portion of the LIDAR triangles
 
 class TileGrid:
     def __init__(self, floor_file):
         self.obj = OBJModel(floor_file)
         if not self.obj.parse():
+            print("Could not load OBJ file", floor_file)
             self.obj = None
             return
 
@@ -41,16 +44,19 @@ class TileGrid:
         return self.obj is not None
 
     def build_grid(self):
-        centre = compute_centre(self.bound)
         scale = self.obj.scale
         dims = self.obj.dims
 
-        print("Map Dims:", scale, dims)
+        centre = m2d.mul(compute_centre(self.bound), dims[0])
+
+        print("Map Dims:", scale, dims, centre)
 
         prim_count = self.obj.get_prim_count()
 
         min_dims = m2d.mul(m2d.sub(self.bound[PB][:-1], self.bound[PA][:-1]), dims[0])
         print(min_dims)
+
+        tile_dims = []
 
         # Find minimum dimensions
         for floor in range(prim_count):
@@ -62,10 +68,12 @@ class TileGrid:
             B = self.obj.get_position(prim[1])[:-1]         # Right Bottom corner
             C = self.obj.get_position(prim[2])[:-1]         # Left Top corner
 
-            lb = [A[X], A[Y]]
-            rt = [B[X], C[Y]]
+            lb = m2d.sub(m2d.mul([A[X], A[Y]], dims[0]), centre)
+            rt = m2d.sub(m2d.mul([B[X], C[Y]], dims[0]), centre)
 
-            tile_delta = m2d.mul(m2d.sub(rt, lb), dims[0])
+            self.poly_arr.append([lb, rt])
+
+            tile_delta = m2d.sub(rt, lb)
             print(floor, A, B, tile_delta)
 
             if tile_delta[X] < min_dims[X]:
@@ -73,11 +81,25 @@ class TileGrid:
             if tile_delta[Y] < min_dims[Y]:
                 min_dims[Y] = tile_delta[Y]
 
+            tile_dims.append(tile_delta)
+
         print(min_dims)
         self.min_dims = min_dims
 
+        # Compute the greatest common divisor for each axis
+        x_dims = list(map(lambda x: x[0], tile_dims))
+        y_dims = list(map(lambda x: x[1], tile_dims))
+        xmin = functools.reduce(lambda x, y: gcd(int(x), int(y)), x_dims)
+        ymin = functools.reduce(lambda x, y: gcd(int(x), int(y)), y_dims)
+
+        print("X Axis GCD:", xmin, x_dims)                  # Seems to be 1 in most cases... will have to be by pixel
+        print("Y Axis GCD:", ymin, y_dims)
+        print("Polygons:", self.poly_arr)
+
 
 if __name__ == '__main__':
+    print("Hello")
+
     filename = '/Users/otgaard/Development/dbm/sim/output/test2_floors.obj'
     grid = TileGrid(filename)
     grid.build_grid()
