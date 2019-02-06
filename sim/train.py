042ec340b9107a3263f91c8d2d8f3b40379783bb
Max Ferguson <maxkferg@gmail.com>
2019-02-06 15:29:11 -0800

    Use random sampling

--- sim/train.py
+++ sim/train.py
@@ -13,10 +13,12 @@ import yaml
 import numpy as np
 import gym
 import argparse
+from random import choice
 from pprint import pprint
 from gym.spaces import Discrete, Box
 from gym.envs.registration import EnvSpec
 from gym.envs.registration import registry
+from ray import tune
 from ray.tune import run_experiments
 from ray.tune.config_parser import make_parser
 from ray.tune import grid_search
@@ -45,26 +47,29 @@ def run(args):
     for experiment, settings in experiments.items():
         settings["env"] = SeekerSimEnv
 
-    experiment = experimental_config(experiment)
+    experiment = experimental_config(experiments)
     pprint(experiments)
     run_experiments(experiments, queue_trials=True, resume="prompt")
 
 
 def experimental_config(config):
-    modify = {
+    changes = {
         'stop': {
             'episode_reward_mean': 1,
             'time_total_s': 3600,
         },
-        'buffer_size': grid_search([500000, 5000000]), 
-        'train_batch_size': grid_search([64, 128, 512]),
-        'lr': grid_search([0.00001, 0.0005, 0.0001]),
-        'tau': grid_search([0.005, 0.001]),
-        'n_step': grid_search([1, 2, 4]),
-        'noise_scale': grid_search([0.1, 0.2, 0.4])
+        'config': {
+            'buffer_size': tune.sample_from([500000, 5000000]), 
+            'train_batch_size': tune.sample_from([64, 128, 512]),
+            'lr': tune.sample_from([0.00001, 0.0005, 0.0001]),
+            'tau': tune.sample_from([0.005, 0.001]),
+            'n_step': tune.sample_from([1, 2, 4]),
+            'noise_scale': tune.sample_from([0.1, 0.2, 0.4])
+        },
     }
-    for k,v in modify.items():
-        config['seeker-td3'][k] = v
+    for context in changes.keys():
+        for k,v in changes[context].items():
+            config['seeker-td3'][context][k] = v
     return config
 
 
